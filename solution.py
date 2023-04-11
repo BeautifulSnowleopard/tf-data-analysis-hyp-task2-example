3aed5a9dd98148b59024316690089b6545d39931
artist_mary <94290722+BeautifulSnowleopard@users.noreply.github.com>
2023-04-11 20:49:22 +0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,7 +5,6 @@ import numpy as np
 chat_id = 123456 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    alpha = 0.04
+    pvalue = kstest(x, y)
+    return pvalue < alpha
