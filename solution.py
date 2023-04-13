866c06052da6eca69b7f2678edb9b3ae2b2f8ae4
artist_mary <94290722+BeautifulSnowleopard@users.noreply.github.com>
2023-04-13 23:20:42 +0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,10 +1,12 @@
 import pandas as pd
 import numpy as np
-
+from scipy.stats import ks_2samp
 
 chat_id = 1897874711 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    alpha = 0.04
-    pvalue = kstest(x, y)
-    return pvalue < alpha
+    D, p = ks_2samp(x, y)
+    if p < 0.04:
+        return True
+    else:
+        return False
